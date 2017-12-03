d82dbb7ca385894530634434c1639962f8d334b0
Dennis Vriend <dnvriend@gmail.com>
2017-12-03 18:46:05 +0100

    removed toSet

--- sbt-sam-plugin/src/main/scala/com/github/dnvriend/sbt/sam/task/ClassifyLambdas.scala
+++ sbt-sam-plugin/src/main/scala/com/github/dnvriend/sbt/sam/task/ClassifyLambdas.scala
@@ -35,16 +35,16 @@ object ClassifyLambdas {
           stage: String): Set[LambdaHandler] = {
 
     val dynamoHandlers = lambdas.map(_.projectClass.cl).filter(annotationPredicate("DynamoHandler"))
-          .map(cl => (cl.getName, cl.getSimpleName, cl.getDeclaredAnnotations.find(_.annotationType().getName.contains("DynamoHandler"))))
-          .flatMap {
-            case (fqcn, simpleName, annotations) => annotations.map(anno => mapAnnoToDynamoHandler(fqcn, simpleName, anno, stage))
-          }.toSet
+      .map(cl => (cl.getName, cl.getSimpleName, cl.getDeclaredAnnotations.find(_.annotationType().getName.contains("DynamoHandler"))))
+      .flatMap {
+        case (fqcn, simpleName, annotations) => annotations.map(anno => mapAnnoToDynamoHandler(fqcn, simpleName, anno, stage))
+      }
 
-        val httpHandlers = lambdas.map(_.projectClass.cl).filter(annotationPredicate("HttpHandler"))
-          .map(cl => (cl.getName, cl.getSimpleName, cl.getDeclaredAnnotations.find(_.annotationType().getName.contains("HttpHandler"))))
-          .flatMap {
-            case (fqcn, simpleName, annotations) => annotations.map(anno => mapAnnoToHttpHandler(fqcn, simpleName, anno, stage))
-          }.toSet
+    val httpHandlers = lambdas.map(_.projectClass.cl).filter(annotationPredicate("HttpHandler"))
+      .map(cl => (cl.getName, cl.getSimpleName, cl.getDeclaredAnnotations.find(_.annotationType().getName.contains("HttpHandler"))))
+      .flatMap {
+        case (fqcn, simpleName, annotations) => annotations.map(anno => mapAnnoToHttpHandler(fqcn, simpleName, anno, stage))
+      }
 
     dynamoHandlers ++ httpHandlers
   }
